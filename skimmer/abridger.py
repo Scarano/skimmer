186889c082e692825fbfc173b26c92bd57777039
Sam Scarano <github@astoundment.com>
2024-01-08 18:14:51 -0500

    A few code comments

--- skimmer/abridger.py
+++ skimmer/abridger.py
@@ -4,14 +4,28 @@ import re
 from skimmer.span_scorer import SpanScorer
 import numpy as np
 
-NULL_PARAGRAPH_PATTERN = r'\w\b\w'
+NULL_PARAGRAPH_PATTERN = r'\w\b\w'  # match nothing; do not detect paragraph breaks
 TEXT_PARAGRAPH_PATTERN = r'\n\s*\n'
 HTML_PARAGRAPH_PATTERN = r'\n*<br/?>\n*'
 
 class Abridger:
+    """
+    Use specified SpanScorer to remove less important/relevant spans.
+    """
 
     def __init__(self, scorer: SpanScorer, keep: float = 0.5, max_spans: Optional[int] = None,
                  paragraph_pattern: str = NULL_PARAGRAPH_PATTERN, ellipsis_string = ' '):
+        """
+        :param scorer: SpanScorer to use to score spans
+        :param keep: proportion of spans to keep (0 to 1)
+        :param max_spans: maximum number of spans to keep (used if non-null, and less than
+            keep * number of spans)
+        :param paragraph_pattern: regular expression pattern used to detect a paragraph breaks.
+            If an omitted section contains at least one match to this pattern, the first match
+            is re-inserted so that the preceding and following (kept) spans are separated by
+            exactly one paragraph break.
+        :param ellipsis_string: string to insert in place of omitted text.
+        """
         self.scorer = scorer
         self.keep = keep
         self.max_spans = max_spans
